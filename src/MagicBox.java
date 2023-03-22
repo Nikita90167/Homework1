cf638d94f5b6ae01211d628b73fb09a546e8da40
Nikita Platonov <fifaakkaynt92@gmail.com>
2023-03-22 12:43:58 +0300

    added the add function

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,2 +1,22 @@
-public class MagicBox {
+public class MagicBox <T> {
+
+    private T[] items;
+
+    public MagicBox(int count) {
+        this.items = (T[]) new Object[count];
+    }
+
+    boolean add(T item) {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i].equals(null)) {
+                items[i] = item;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    T pick(){
+        return ;
+    }
 }
